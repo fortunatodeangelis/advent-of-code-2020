edcffa599fdfe2b7ac904707f20f4bac0ba34eb1
Nunzio Marfè <nunzio.marfe@leadtech.it>
2020-12-06 19:44:26 +0100

    2nd day;

---
+++ day2/part2/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$fileInputRows = file('../input.txt');
+/*$fileInputRows = [
+    "1-3 a: abcde",
+    "1-3 b: cdefg",
+    "2-9 c: ccccccccc"
+];*/
+
+
+$rightPasswordCounter = 0;
+foreach ($fileInputRows as $pwPolicy) {
+    /** Formatto le variabili nel modo più adatto */
+    $passwordSettings = explode(" ", $pwPolicy);
+    $passwordSettings[0] = explode("-", $passwordSettings[0]);
+    $passwordSettings[1] = $passwordSettings[1][0];
+
+    if (($passwordSettings[2][(int)$passwordSettings[0][0] - 1] === $passwordSettings[1] && $passwordSettings[2][(int)$passwordSettings[0][1] - 1] !== $passwordSettings[1]) || ($passwordSettings[2][(int)$passwordSettings[0][0] - 1] !== $passwordSettings[1] && $passwordSettings[2][(int)$passwordSettings[0][1] - 1] === $passwordSettings[1])) {
+        $rightPasswordCounter++;
+    }
+}
+
+echo $rightPasswordCounter;
