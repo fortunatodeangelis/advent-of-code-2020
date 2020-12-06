f86ed9cf1074c1e1cbec7f28cc48ab53014a846d
Nunzio Marfè <nunzio.marfe@leadtech.it>
2020-12-06 21:46:08 +0100

    day4

---
+++ day4/part1/index.php
@@ -0,0 +1,44 @@
+<?php
+
+$fileInputRows = file('../input.txt');
+
+$arrayRequiredFields = [
+    "byr",
+    "iyr",
+    "eyr",
+    "hgt",
+    "hcl",
+    "ecl",
+    "pid",
+    //"cid",
+];
+
+$tmpRows = [];
+$key = 0;
+foreach ($fileInputRows as $row) {
+    if (empty(trim($row))) {
+        $key++;
+        continue;
+    }
+    $tmpRows[$key][] = $row;
+}
+
+$orderedRows = [];
+foreach ($tmpRows as $k => $passportRows) {
+    foreach (explode(" ", implode(" ", $passportRows)) as $item) {
+        $arrayFieldValue = explode(":", $item);
+        $orderedRows[$k][$arrayFieldValue[0]] = $arrayFieldValue[1];
+    }
+}
+
+$validPassport = 0;
+foreach ($orderedRows as $fields) {
+    if (!array_diff_key(array_flip($arrayRequiredFields), $fields)) {
+        $validPassport++;
+    }
+}
+
+echo "<pre>";
+print_r($orderedRows);
+
+echo $validPassport;
